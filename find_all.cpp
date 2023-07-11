2f334746ad1265e943cf9fb770b71a4d44955ca9
mayanknimcet188 <me.mayank.0602@gmail.com>
2023-07-11 13:49:42 +0530

    Generalizing the test to take test name and inputs

--- find_all.cpp
+++ find_all.cpp
@@ -37,19 +37,18 @@ OutputIterator find_all(InputIterator first, InputIterator last, OutputIterator 
 // Test code
 // Initialize a vector container with some values containing 4 at 2 different placs
 // after running the find_all algorithm we check the returned iteraor distances to be the specified indexes, report success and failure according to the same
-int main()
+
+void find_all_test(std::string const &test_name, std::string const &text, std::pair<std::vector<int>, std::vector<int>> test_input)
 {
-    std::cout << "test: find all index of a particular element, result: ";
-    std::vector<int> values{3, 4, 5, 4, 5, 6, 5, 8};
+    std::cout << "test: " << test_name << " result: ";
     std::vector<std::vector<int>::iterator> matches;
-    find_all(std::begin(values), std::end(values), std::back_inserter(matches), [](int i)
+    find_all(std::begin(test_input.first), std::end(test_input.second), std::back_inserter(matches), [](int i)
              { return i == 5; });
     std::vector<int> matchIndexes;
-    for (auto it : matches)
-    {
-        matchIndexes.push_back(it - std::cbegin(values));
-    }
-    if (matchIndexes == std::vector<int>{2, 4, 6})
+    auto getIndexes = [&matches, &test_input, &matchIndexes]()
+    { for(auto it : matches) { matchIndexes.push_back(it - std::cbegin(test_input.first)); } };
+    getIndexes();
+    if (matchIndexes == test_input.second)
     {
         std::cout << "Success\n";
     }
@@ -57,4 +56,11 @@ int main()
     {
         std::cout << "Failure\n";
     }
+}
+
+int main()
+{
+    std::vector<int> values{3, 4, 5, 4, 5, 6, 5, 8};
+    std::vector<int> indexes{2, 4, 6};
+    find_all_test("Find all algorithm", "find all index of a particular element", std::make_pair(values, indexes));
 }
